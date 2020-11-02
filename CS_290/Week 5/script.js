afdc646aca1cc54700421522b179b3909610b245
Scott Fleishman <scottyfleishman@gmail.com>
2020-11-02 13:10:37 -0800

    start on hw5

--- CS_290/Week 5/script.js
+++ CS_290/Week 5/script.js
@@ -1,2 +1,9 @@
 // JS For Week 5's stuff
-    
+
+var createTable = document.createElement("table");
+for(var i = 0; i < 3; i++){
+    var newItem = document.createElement("tr");
+    newItem.textContent = "I am item " + i + ".";
+    createTable.appendChild(newItem);
+}
+document.body.appendChild(createTable);
